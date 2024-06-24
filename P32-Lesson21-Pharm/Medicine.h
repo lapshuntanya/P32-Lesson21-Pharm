bafba360326f85619571daba569137e3cc3d628e
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-24 17:13:21 +0300

    add class Medicine

---
+++ P32-Lesson21-Pharm/Medicine.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+enum TYPES {
+	other,
+	pills = 1,	//пігулки
+	capsules,	//капсули
+	syrop		//сироп
+};
+
+class Medicine
+{
+	char* _title;
+	TYPES _type;
+	float _price;
+	char* _country;
+
+public:
+	Medicine();
+	Medicine(const char* title, TYPES type, float price,
+		const char* country);
+	~Medicine();
+
+	void setTitle(const char* title);
+	void setType(TYPES type);
+	void setPrice(float price);
+	void setCountry(const char* country);
+
+	const char* getTitle()const;
+	TYPES getType()const;
+	float getPrice()const;
+	const char* getCountry()const;
+
+	void showInfo()const;
+};
+
